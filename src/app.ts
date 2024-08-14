4f563f1089a862e2e7ff562aa756f5f0e27f8e65
Virginie <vb@ikimo9.com>
2024-08-14 14:15:56 +0200

    ajout middleware + routes

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import todoRoutes from './routes/todos';
 
 const app = express();
+
+app.use('/todos', todoRoutes);
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+  response.status(500).json({message: error.message});
+});
 
 app.listen(3000);
